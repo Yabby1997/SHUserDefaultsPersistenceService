2ffdbca1915b57ec02c803611638f2f702a9f141
Yabby1997 <yabby1997@gmail.com>
2023-06-08 23:52:52 +0900

    Fix unopened apis

--- Sources/SHUserDefaultsPersistenceService/SHUserDefaultsPersistenceService.swift
+++ Sources/SHUserDefaultsPersistenceService/SHUserDefaultsPersistenceService.swift
@@ -8,18 +8,26 @@
 import Foundation
 import SHPersistenceServiceInterface
 
-final class SHUserDefaultsPersistenceService: SHPersistenceServiceInterface {
+public final class SHUserDefaultsPersistenceService: SHPersistenceServiceInterface {
+
+    // MARK: - Dependencies
+
     private let decoder = JSONDecoder()
     private let encoder = JSONEncoder()
 
-    func load<DTO: Codable>(key: String) -> [DTO] {
+    // MARK: - Initializers
+
+    public init() {}
+
+    // MARK: - Public Methods
+
+    public func load<DTO: Codable>(key: String) -> [DTO] {
         guard let data = UserDefaults.standard.data(forKey: key),
               let dto = try? decoder.decode([DTO].self, from: data) else { return [] }
         return dto
     }
 
-    @discardableResult
-    func save<DTO: Codable>(key: String, objects: [DTO]) -> Bool {
+    public func save<DTO: Codable>(key: String, objects: [DTO]) -> Bool {
         guard let data = try? encoder.encode(objects) else { return false }
         UserDefaults.standard.setValue(data, forKey: key)
         return true
